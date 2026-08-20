78fe2344d043fafeff83d46615830cd10c03eeab
let-if <letifylkal6@gmail.com>
2026-08-20 01:22:14 -0700

    Fix Prisma v7 config and update Render build command

--- backend/prisma.config.ts
+++ backend/prisma.config.ts
@@ -1,17 +1,18 @@
-// import "dotenv/config";
-// import { defineConfig, env } from "@prisma/config";
+
+// import { defineConfig } from "prisma/config";
 
 // export default defineConfig({
-//   schema: "prisma/schema.prisma",
+//   schema: "./prisma/schema.prisma",
 //   datasource: {
-//     url: env("DATABASE_URL"),
+//     url: "postgresql://neondb_owner:npg_lXk8eag3NdDc@ep-mute-violet-ay4qeky0.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require",
 //   },
 // });
-import { defineConfig } from "prisma/config";
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "./prisma/schema.prisma",
   datasource: {
-    url: "postgresql://neondb_owner:npg_lXk8eag3NdDc@ep-mute-violet-ay4qeky0.c-5.us-east-2.aws.neon.tech/neondb?sslmode=require",
+    url: env("DATABASE_URL"),
   },
 });
